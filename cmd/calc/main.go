f3d47ba9d18d66c412b88496e46773f65cb08a9c
Paul Crane <paul@acheronsoft.com>
2025-02-06 23:12:32 -0600

    Add several math functions for environment map

--- cmd/calc/main.go
+++ cmd/calc/main.go
@@ -11,7 +11,35 @@ import (
 func main() {
 	args := os.Args[1:]
 	env := map[string]interface{}{
-		"pi": math.Pi,
+		"pi":        math.Pi,
+		"round":     math.Round,
+		"ceil":      math.Ceil,
+		"floor":     math.Floor,
+		"abs":       math.Abs,
+		"sqrt":      math.Sqrt,
+		"exp":       math.Exp,
+		"exp2":      math.Exp2,
+		"log":       math.Log,
+		"log10":     math.Log10,
+		"log1p":     math.Log1p,
+		"log2":      math.Log2,
+		"logb":      math.Logb,
+		"pow":       math.Pow,
+		"sin":       math.Sin,
+		"cos":       math.Cos,
+		"tan":       math.Tan,
+		"asin":      math.Asin,
+		"acos":      math.Acos,
+		"atan":      math.Atan,
+		"atan2":     math.Atan2,
+		"max":       math.Max,
+		"min":       math.Min,
+		"mod":       math.Mod,
+		"modf":      math.Modf,
+		"remainder": math.Remainder,
+		"cbrt":      math.Cbrt,
+		"fma":       math.FMA,
+		"hypot":     math.Hypot,
 	}
 
 	if len(args) > 0 {
